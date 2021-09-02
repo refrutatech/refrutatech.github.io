1417cce2f45b7eae3a4e2f8857d61d7fd5c74cd3
refrutatech <90005937+refrutatech@users.noreply.github.com>
2021-09-02 17:25:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Sidebar botão menu
+var mySidebar = document.getElementById("mySidebar");
+
+function w3_open() {
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+  } else {
+    mySidebar.style.display = 'block';
+  }
+}
+
+// fechar sidebar
+function w3_close() {
+    mySidebar.style.display = "none";
+}
